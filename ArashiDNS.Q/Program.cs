3416d812e17191a90d8793218e34247947814fd3
Milkey Tan <819774989@qq.com>
2020-03-23 20:13:58 +0800

    添加项目文件。

---
+++ ArashiDNS.Q/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ArashiDNS.Q
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
