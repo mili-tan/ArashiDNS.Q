e094f1e517d8a49810a18fcefb57f28b9ba3cb4c
Milkey Tan <819774989@qq.com>
2020-03-23 20:38:05 +0800

    update

--- ArashiDNS.Q/Program.cs
+++ ArashiDNS.Q/Program.cs
@@ -1,7 +1,10 @@
 ﻿using System;
+using System.ComponentModel;
+using System.Net;
 using QuicNet;
-using QuicNet.Context;
 using System.Text;
+using ARSoft.Tools.Net.Dns;
+using QuicNet.Connections;
 
 namespace ArashiDNS.Q
 {
@@ -9,18 +12,37 @@ namespace ArashiDNS.Q
     {
         static void Main(string[] args)
         {
-            QuicListener listener = new QuicListener(11000);
+            DNSEncoder.Init();
+            var listener = new QuicListener(11000);
             listener.Start();
             while (true)
             {
-                QuicConnection client = listener.AcceptQuicClient();
+                var client = listener.AcceptQuicClient();
 
-                client.OnDataReceived += (c) => {
-                    byte[] data = c.Data;
-                    Console.WriteLine("Data received: " + Encoding.UTF8.GetString(data));
-                    c.Send(Encoding.UTF8.GetBytes("Echo!"));
+                client.OnDataReceived += c => {
+                    var msg = c.Data;
+                    using (var bgWorker = new BackgroundWorker())
+                    {
+                        bgWorker.DoWork += (sender, eventArgs) =>
+                        {
+                            var dnsQMsg = DnsMessage.Parse(msg);
+                            var dnsRMsg =
+                                new DnsClient(IPAddress.Parse("8.8.8.8"), 1000).SendMessage(dnsQMsg);
+                            c.Send(DNSEncoder.Encode(dnsRMsg));
+
+                            Console.ForegroundColor = ConsoleColor.Cyan;
+                            dnsQMsg.Questions.ForEach(o => Console.WriteLine("Qu:" + o));
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            dnsRMsg.AnswerRecords.ForEach(o => Console.WriteLine("An:" + o));
+                            Console.ForegroundColor = ConsoleColor.DarkGreen;
+                            dnsRMsg.AuthorityRecords.ForEach(o => Console.WriteLine("Au:" + o));
+                            Console.ForegroundColor = ConsoleColor.Green;
+                        };
+                        bgWorker.RunWorkerAsync();
+                    }
                 };
-            }
+            };
+            
         }
     }
 }
